6a900d38994e7ffa5af67d3e04e9c7351a8e42dc
ding1226 <125621604+ding1226@users.noreply.github.com>
2024-06-21 01:32:16 +0800

    Create client.c

---
+++ client.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define BUFFER_SIZE 1024
+
+void *receive_messages(void *client_socket) {
+    int sock = *(int *)client_socket;
+    char buffer[BUFFER_SIZE];
+    int bytes_received;
+
+    while ((bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[bytes_received] = '\0';
+        printf("%s", buffer);
+    }
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server_ip> <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    char *server_ip = argv[1];
+    int port = atoi(argv[2]);
+
+    int client_socket;
+    struct sockaddr_in server_addr;
+
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Could not create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address/Address not supported");
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to server at %s:%d\n", server_ip, port);
+
+    pthread_t receive_thread;
+    pthread_create(&receive_thread, NULL, receive_messages, &client_socket);
+    pthread_detach(receive_thread);
+
+    char message[BUFFER_SIZE];
+    while (1) {
+        if (fgets(message, sizeof(message), stdin) != NULL) {
+            if (strncmp(message, "exit", 4) == 0) {
+                printf("Client shutting down...\n");
+                close(client_socket);
+                exit(0);
+            }
+            send(client_socket, message, strlen(message), 0);
+        }
+    }
+
+    return 0;
+}
